fef5b9a00fbd91b7f8e65eac37ddba9d0891daf8
Corey Peck <coreypeck@gmail.com>
2016-10-30 21:16:19 -0500

    added show hide functionality for each respective category

--- server/public/scripts/controllers/userController.js
+++ server/public/scripts/controllers/userController.js
@@ -1,6 +1,5 @@
 myApp.controller('userController', ['$scope', '$http', '$animate', function($scope, $http, $animate) {
     $scope.showSidebar = function() {
-        console.log("fuck");
         if ($scope.sidebar) {
             $scope.sidebar = false;
         } else {
@@ -25,18 +24,83 @@ myApp.controller('userController', ['$scope', '$http', '$animate', function($sco
         "Truss",
         "Weight Conv"
     ];
-    $scope.showingArray = [];
+
     $scope.makeTrue = function(category){
+      makeFalse();
       console.log(category);
-      $scope.showingArray = [];
-      $scope.showingArray.push(category);
+      switch(category){
+        case "2-Point Bridles":
+        $scope.twoPointBridles = true;
+        break;
+        case "3-Point Bridles":
+        $scope.threePointBridles = true;
+        break;
+        case "Avg Light Lvl (area)":
+        $scope.avgLightArea = true;
+        break;
+        case "Beam":
+        $scope.beam = true;
+        break;
+        case "Cable":
+        $scope.cable = true;
+        break;
+        case "Candlepower":
+        $scope.candlepower = true;
+        break;
+        case "Footcandles":
+        $scope.footcandles = true;
+        break;
+        case "Footcandles & Lms":
+        $scope.footcandlesAndLms = true;
+        break;
+        case "Horizontal Breastline":
+        $scope.horizontalBreastline = true;
+        break;
+        case "Length Conv":
+        $scope.lengthConv = true;
+        break;
+        case "Light Lvl (point)":
+        $scope.lightLvlPoint = true;
+        break;
+        case "Mechanical Adv":
+        $scope.mechanicalAdv = true;
+        break;
+        case "Point Load":
+        $scope.pointLoad = true;
+        break;
+        case "Power":
+        $scope.power = true;
+        break;
+        case "Resultant Forces":
+        $scope.resultantForces = true;
+        break;
+        case "Truss":
+        $scope.truss = true;
+        break;
+        case "Weight Conv":
+        $scope.weightConv = true;
+        break;
+      }
+    }
+    function makeFalse(){
+      $scope.twoPointBridles = false;
+      $scope.threePointBridles = false;
+      $scope.avgLightArea = false;
+      $scope.beam = false;
+      $scope.cable = false;
+      $scope.candlepower = false;
+      $scope.footcandles = false;
+      $scope.footcandlesAndLms = false;
+      $scope.horizontalBreastline = false;
+      $scope.lengthConv = false;
+      $scope.lightLvlPoint = false;
+      $scope.mechanicalAdv = false;
+      $scope.pointLoad = false;
+      $scope.power = false;
+      $scope.resultantForces = false;
+      $scope.truss = false;
+      $scope.weightConv = false;
+
       $scope.sidebar = false;
-      $scope.categoryArray.forEach(function(string){
-        if($scope.showingArray[0] == category){
-          return;
-        }else{
-          $scope[string] = false;
-        }
-      });
-    };
+    }
 }]);
